2ac4edd870af670d3d08703e4444ee252dc5fe9d
ohheyimjen <jendority@gmail.com>
2019-04-12 11:23:45 -0700

    added nav bar in app.js

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
 import axios from "axios";
+import {NavLink, Link, Route} from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -27,23 +28,52 @@ class App extends Component {
       });
   }
 
-  // addSmurf = smurf => {
-
-  //   axios
-  //     .post('http://localhost:3333/smurfs')
-  //     .then(response => this.setState({
-  //       smurfs: response.data
-  //     }))
-  //     .catch(error => console.log(error));
-  // };
+// const Navigation = (props) => {
+//     return (
+//       <div>
+//         <div className="App">
+//           <h1>Welcome to the Smurf Village!</h1>
+//           <div>
+//             <Link to="/" className="link-home">Home</Link>
+//           </div>
+//           <div>
+//             <Link to="/smurfs">Meet the Smurfs</Link>
+//           </div>
+//           <div>
+//             <Link to="/smurf-form">Add a Smurf</Link>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink to="/smurf-form" className="nav">Add a Smurf</NavLink>
+        <NavLink to="/smurfs" className="nav">Go to Smurf Village!</NavLink>
+        <Link to="/smurfs" className="nav">Smurfs</Link>
+        <Link to="/smurf-form">Smurf Form</Link>
+        
+        <Route path="/smurf-form" 
+          render={props => (
+          <SmurfForm 
+            {...props}
+            addSmurf={this.addSmurf}
+          />
+          )}
+          />
+
+        <Route path="/smurfs"  
+          render={props => (
+          <Smurfs 
+          {...props}
+            smurfs={this.state.smurfs} 
+        />
+        )}
+        />
       </div>
     );
   }
--- village/src/index.js
+++ village/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router} from 'react-dom-router';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
 import './index.css';
